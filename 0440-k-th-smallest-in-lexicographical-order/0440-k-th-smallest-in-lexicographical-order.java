bc7710f56ebdff5b60caedeca9b2de461e78f1e2
Piyush Kumar <piyushcaptian0@gmail.com>
2024-09-23 02:34:27 +0530

    Time: 0 ms (100%), Space: 40.5 MB (22.9%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int Count(long curr,long next,int n){
+        int countNum=0;
+        while(curr <= n){
+            countNum +=Math.min(next,(long) n+1)-curr;
+
+            curr *=10;
+            next *= 10;
+        }
+        return countNum;
+    }
+    public int findKthNumber(int n, int k) {
+        int curr=1;
+        k -=1; //sincewe start from 1st number (1), we need k-1 more numbwer
+
+        while(k>0){
+            int count=Count(curr,curr+1,n);
+            if(count<=k){
+                curr++;
+                k -=count; //skiping the number under curr prefix tree
+            }
+            else{
+                curr *=10;
+                k -=1;
+            }
+        }
+        return curr;
+        
+    }
+}

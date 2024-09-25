f88d9df94e1593b0a357a4251917a218b93e9b10
Piyush Kumar <piyushcaptian0@gmail.com>
2024-09-25 07:27:26 +0530

    Updating 2nd approch

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
@@ -1,3 +1,6 @@
+//Approach 1 - Brute Force
+//T.C : O(m⋅log10​M + n⋅log10​N)
+//S.C : O(m⋅log10​M)
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
         Set <Integer> st=new HashSet<>();
@@ -19,5 +22,60 @@ class Solution {
         }
         return count;
         
+    }
+}
+//Approch 2
+//using trie(prefix Tree)
+//Time Complexity :- 0(m*d+n*d) where m and n is length of arr1 and arr2 respectively and d is length of word or number
+//space Complexity :- 0(m*d)
+class Solution {
+    static class trieNode{
+        trieNode []child;
+        trieNode(){
+            child=new trieNode[10];
+        }
+    }
+
+    public void insert(int num,trieNode root) {
+        trieNode curr=root;
+        String numStr=Integer.toString(num);
+        for(int i=0;i<numStr.length();i++){
+            int idx=numStr.charAt(i)-'0';
+            if(curr.child[idx]==null){
+                curr.child[idx]= new trieNode();
+            }
+            curr=curr.child[idx];
+        }   
+    }
+
+    public int search(int num, trieNode root) {
+        trieNode curr = root;
+        String numStr = Integer.toString(num);
+        int length = 0;
+
+        for(int i=0;i<numStr.length();i++){
+            int idx=numStr.charAt(i)-'0';
+            if (curr.child[idx] != null) {
+                length++;
+                curr = curr.child[idx];
+            } else {
+                break;
+            }
+        }
+        return length;
+    }
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        trieNode root=new trieNode();
+
+        for (int num : arr1) {
+            insert(num, root);
+        }
+
+        int result = 0;
+        for (int num : arr2) {
+            result = Math.max(result, search(num, root));
+        }
+        return result;    
     }
 }

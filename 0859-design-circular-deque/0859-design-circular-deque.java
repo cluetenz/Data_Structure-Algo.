3161e3b65c29141cec38d208911d451687fc8a92
Piyush Kumar <piyushcaptian0@gmail.com>
2024-09-29 01:40:14 +0530

    Update 0859-design-circular-deque.java

--- 0859-design-circular-deque/0859-design-circular-deque.java
+++ 0859-design-circular-deque/0859-design-circular-deque.java
@@ -1,3 +1,6 @@
+//Approch :- Using Array
+//Time Complexity :- O(1)
+//Space Complexity :- O(n)
 class MyCircularDeque {
     private LinkedList<Integer> deq;  // Using LinkedList to store deque elements
     private int cnt;                  // Current number of elements in the deque
@@ -73,6 +76,86 @@ class MyCircularDeque {
         return cnt==k;  
     }
 }
+
+//Approch :- Using doubly-Linked List
+//Time Complexity :- O(1)
+//Space Complexity :- O(n)
+class MyCircularDeque {
+    private LinkedList<Integer> deq;  // Using LinkedList to store deque elements
+    private int cnt;                  // Current number of elements in the deque
+    private int k;                    // Maximum capacity of the deque
+
+    public MyCircularDeque(int k) {
+        this.k = k;                  
+        this.deq = new LinkedList<>(); 
+        this.cnt = 0;                  
+        
+    }
+    
+    public boolean insertFront(int value) {
+        if (cnt == k) {
+            return false;              
+        }
+        deq.addFirst(value);         
+        cnt++;                         
+        return true;  
+    }
+    
+    public boolean insertLast(int value) {
+        if (cnt == k) {
+            return false;             
+        }
+        deq.addLast(value);            
+        cnt++;                         
+        return true;
+        
+    }
+    
+    public boolean deleteFront() {
+        if (cnt == 0) {
+            return false;             
+        }
+        deq.removeFirst();             
+        cnt--;                         
+        return true;
+        
+    }
+    
+    public boolean deleteLast() {
+        if (cnt == 0) {
+            return false;              
+        }
+        deq.removeLast();             
+        cnt--;                         
+        return true;
+        
+    }
+    
+    public int getFront() {
+        if (cnt == 0) {
+            return -1;                
+        }
+        return deq.getFirst();  
+        
+    }
+    
+    public int getRear() {
+         if (cnt == 0) {
+            return -1;             
+        }
+        return deq.getLast();
+        
+    }
+    
+    public boolean isEmpty() {
+        return cnt==0; 
+    }
+    
+    public boolean isFull() {
+        return cnt==k;  
+    }
+}
+
 
 /**
  * Your MyCircularDeque object will be instantiated and called as such:

97880ae9498363ae99b09660b77e00764e47e707
Piyush Kumar <piyushcaptian0@gmail.com>
2024-10-04 11:17:55 +0530

    Update 2581-divide-players-into-teams-of-equal-skill.java

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -1,3 +1,6 @@
+//brute force
+//T.C:-O(n log n)
+//S.C:- O(1)
 class Solution {
     public long dividePlayers(int[] skill) {
         Arrays.sort(skill);
@@ -15,5 +18,34 @@ class Solution {
             j--;
         }
         return chemistry;
+    }
+}
+
+//T.C:- O(n)
+//S.C:- O(100000)
+class Solution {
+    public long dividePlayers(int[] skill) {
+        int[] arr=new int[100000];
+        int sum=0;
+        for(int num:skill){
+            sum+=num;
+            arr[num]++;
+        }
+        int teams=skill.length/2;
+        if(sum%teams!=0){
+            return -1;
+        }
+        int target=sum/teams;
+        long chemistry=0;
+        for(int i=0;i<skill.length;i++){
+            int currSkill=skill[i];
+            int remSkill=target-currSkill;
+            if(arr[remSkill]<=0){
+                return -1;
+            }
+            chemistry+=(long)currSkill*(long)remSkill;
+            arr[remSkill] -=1;
+        }
+        return chemistry/2;  
     }
 }

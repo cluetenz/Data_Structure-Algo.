90ab1069ec539efbe569c5a5d8cff968e26b7be2
Piyush Kumar <piyushcaptian0@gmail.com>
2024-10-06 23:04:44 +0530

    Update 1923-sentence-similarity-iii.java

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
@@ -1,3 +1,6 @@
+//Approach:- using pointers
+//T.C : O(m+n)
+//S.C : O(m+n)
 class Solution {
     public boolean areSentencesSimilar(String s1, String s2) {
         if(s1.length()<s2.length()){
@@ -25,5 +28,32 @@ class Solution {
             j--;
         }
         return l<k;
+    }
+}
+
+//approch:- using deque
+//T.C:- O(m+n)
+//S.C:- O(m+n)
+class Solution {
+    public boolean areSentencesSimilar(String s1, String s2) {
+        if(s1.length()<s2.length()){
+            String temp=s1;
+            s1=s2;
+            s2=temp;
+        }
+        Deque<String> deq1 = new LinkedList<>(Arrays.asList(s1.split(" ")));
+        Deque<String> deq2 = new LinkedList<>(Arrays.asList(s2.split(" ")));
+
+        //prefix check
+        while(!deq1.isEmpty() && !deq2.isEmpty() && deq1.peekFirst().equals(deq2.peekFirst())){
+            deq1.pollFirst();
+            deq2.pollFirst();
+        }
+        //postfix check
+        while (!deq1.isEmpty() && !deq2.isEmpty() && deq1.peekLast().equals(deq2.peekLast())) {
+            deq1.pollLast();
+            deq2.pollLast();
+        }
+        return deq2.isEmpty();
     }
 }

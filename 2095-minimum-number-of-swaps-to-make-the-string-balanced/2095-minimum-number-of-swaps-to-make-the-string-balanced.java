4e188ab92ac0103965862d05bcb53a105d5b1586
Piyush Kumar <piyushcaptian0@gmail.com>
2024-10-08 23:20:27 +0530

    Update 2095-minimum-number-of-swaps-to-make-the-string-balanced.java

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,3 +1,6 @@
+//Approch:- using stack
+//T.C:- O(n)
+//S.C:- O(n)
 class Solution {
     public int minSwaps(String s) {
         Stack<Character> st=new Stack<>();
@@ -11,6 +14,27 @@ class Solution {
             }
         }
         return (st.size()+1)/2;
+        
+    }
+}
+
+
+//Approch:- using variable instead of stack
+//T.C:- O(n)
+//S.C:- O(1)
+class Solution {
+    public int minSwaps(String s) {
+        int n=s.length();
+        int size=0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='['){
+                size++;
+            }
+            else if(size>0){
+                size--;
+            }
+        }
+        return (size+1)/2;
         
     }
 }

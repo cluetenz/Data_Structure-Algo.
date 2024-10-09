e6b15ed672ff08a7018abe038c252533b8b03e6d
Piyush Kumar <piyushcaptian0@gmail.com>
2024-10-09 12:00:43 +0530

    Update 0957-minimum-add-to-make-parentheses-valid.java

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,3 +1,6 @@
+//Approch:- using Stack
+//T.C:- O(n)
+//S.C:- O(n)
 class Solution {
     public int minAddToMakeValid(String s) {
         Stack <Character> st=new Stack <>();
@@ -16,6 +19,30 @@ class Solution {
             }
         }
         return openBra + st.size();
+        
+    }
+}
+//Approch;- without using stack
+//T.C:-O(n)
+//S.C:-O(1)
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int size=0;
+        int n= s.length();
+        int openBra=0;
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                size++;
+            }
+            else if(size>0){
+                size--;
+            }
+            else{
+                openBra++;
+            }
+        }
+        return openBra + size;
         
     }
 }

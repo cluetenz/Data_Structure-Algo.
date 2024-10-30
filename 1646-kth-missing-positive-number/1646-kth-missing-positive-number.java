2f247da6ffd3250092153baece3a58528d450edf
Piyush Kumar <piyushcaptian0@gmail.com>
2024-10-31 01:45:27 +0530

    Update 1646-kth-missing-positive-number.java

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -1,3 +1,6 @@
+//Approch:- Using simple linear Search
+//T.C:- O(n)
+//S.C:-O(1)
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         int missCount = 0, search = 1, i = 0;
@@ -15,3 +18,30 @@ class Solution {
         return search;
     }
 }
+
+//Approch 2:- Using Binary Search
+//T.C:- O(log n)
+//S.C:- O(1)
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+
+        // Perform binary search
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Missing numbers up to arr[mid]
+            int missingCount = arr[mid] - (mid + 1);
+
+            if (missingCount < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        // left points to the index where the k-th missing number would be
+        return left + k;
+    }
+}
+

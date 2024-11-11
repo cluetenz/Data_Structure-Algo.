aeb2fef1b6a0c9d7a1f820c9c1965bd263650571
Piyush Kumar <piyushcaptian0@gmail.com>
2024-11-11 21:09:51 +0530

    Update 2716-prime-subtraction-operation.java

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.java
@@ -38,3 +38,48 @@ class Solution {
         return true;
     }
 }
+
+//Approch 2
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    boolean []prime=new boolean[1000];
+    public void isPrime() {
+        Arrays.fill(prime,true);
+        prime[0]=false;
+        prime[1]=false;
+        for (int i = 2; i * i <= 1000; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < 1000; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+    }
+    
+    public boolean primeSubOperation(int[] nums) {
+        int n = nums.length;
+        isPrime();
+        
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                continue;
+            }
+            for(int p=2;p<nums[i];p++){
+                if(!prime[p]){
+                    continue;
+                }
+                if(nums[i]-p<nums[i+1]){
+                    nums[i]-=p;
+                    break;
+                }
+            }
+            if(nums[i]>=nums[i+1]){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}

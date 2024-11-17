ee1a04b92b39f8fa3c1f590dd6c5fef45baebd24
Piyush Kumar <piyushcaptian0@gmail.com>
2024-11-17 11:18:18 +0530

    Update 3522-find-the-power-of-k-size-subarrays-i.java

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -28,3 +28,48 @@ class Solution {
     }
 
 }
+
+//Approch 2:- Sliding window
+//T.C:-O(n)
+//S.C:-0(1)
+
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n=nums.length;
+        int ans []=new int [n-k+1];
+        int count=1;
+        Arrays.fill(ans,-1);
+
+        //1st window
+        for(int i=1;i<k;i++){
+            if(nums[i-1]+1==nums[i]){
+                count++;
+            }else{
+                count=1;
+            }
+        }
+        if(count==k){
+            ans[0]=nums[k-1];
+        }
+
+        int i = 1;
+        int j = k;
+
+        // Process the sliding window
+        while (j < n) {
+            if (nums[j] == nums[j - 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count >= k) {
+                ans[i] = nums[j];
+            }
+
+            i++;
+            j++;
+        }
+        return ans;
+    }
+}

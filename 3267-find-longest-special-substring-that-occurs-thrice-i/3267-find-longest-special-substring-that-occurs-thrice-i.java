cc34550016023d6e766ecfb8f5a8eebc3fbf5a2f
Piyush Kumar <piyushcaptian0@gmail.com>
2024-12-11 03:28:33 +0530

    Update 3267-find-longest-special-substring-that-occurs-thrice-i.java

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.java
@@ -25,3 +25,70 @@ class Solution {
         return result;    
     }
 }
+
+//Approch:-Brute Force(better)
+//T.C:-O(n^2)
+class Solution {
+    public int maximumLength(String s) {
+        int n=s.length();
+        HashMap<Pair<Character,Integer>,Integer> hm=new HashMap<>();
+        int len=0;
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            len=0;
+            for(int j=i;j<n;j++){
+                if(s.charAt(j)==ch){
+                    len++;
+                    Pair<Character, Integer> key = new Pair<>(ch, len);
+                    hm.put(key,hm.getOrDefault(key,0)+1);
+                }else{
+                    break;
+                }
+            }
+        }
+        int result=-1;
+        for (Map.Entry<Pair<Character,Integer>, Integer> entry : hm.entrySet()) {
+            if(entry.getValue()>=3){
+                result=Math.max(result,entry.getKey().getValue());
+            }
+        }
+        return result;    
+    }
+}
+
+//Approch:-Optimal Approch using 2-d array
+//T.C:-O(n)
+class Solution {
+    public int maximumLength(String s) {
+        int n=s.length();
+        int len=0;
+        int [][] matrix=new int[26][n+1];
+        char prevChar=s.charAt(0);
+
+        // filling the frequency matrix
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch==prevChar){
+                len++;
+                matrix[ch-'a'][len]+=1;
+            }
+            else{
+                len=1;
+                matrix[ch-'a'][len]+=1;
+                prevChar=ch;
+            }
+        }
+        int result=-1;
+        for (int i = 0; i < 26; i++) {
+            int cumSum = 0;
+            for (int j = n; j >= 1; j--) {
+                cumSum += matrix[i][j];
+                if (cumSum >= 3) {
+                    result = Math.max(result, j);
+                    break;
+                }
+            }
+        }
+        return result;    
+    }
+}

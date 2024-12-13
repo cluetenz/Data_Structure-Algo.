8c2be6161bed7a09b2fa339c85d34e20445297ac
Piyush Kumar <piyushcaptian0@gmail.com>
2024-12-13 18:28:44 +0530

    Update 2695-find-score-of-an-array-after-marking-all-elements.java

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -1,3 +1,5 @@
+
+//Approch:-Using sorting array with indix 
 class Solution {
     public long findScore(int[] nums) {
         int n=nums.length;
@@ -18,6 +20,37 @@ class Solution {
                 
                 if (idx + 1 < n) visited[idx + 1] = true;
                 if (idx - 1 >= 0) visited[idx - 1] = true;
+            }
+        }
+        return score;
+    }
+}
+
+//Approch:- Using Priority Queue
+//T.C:-O(nlogn)
+//S.C:-0(n)
+
+class Solution {
+    public long findScore(int[] nums) {
+        int n=nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[0]==b[0])?Integer.compare(a[1],b[1]):Integer.compare(a[0],b[0]));
+        for(int i=0;i<n;i++){
+            pq.add(new int[]{nums[i],i});
+        }
+        long score=0;
+        boolean []visited=new boolean [n];
+        while(!pq.isEmpty()){
+            int[] arr=pq.poll();
+            int value=arr[0];
+            int idx=arr[1];
+            if(!visited[idx]){
+                score+=value;
+                if(idx+1<n){
+                    visited[idx+1]=true;
+                }
+                if(idx-1>=0){
+                    visited[idx-1]=true;
+                }
             }
         }
         return score;

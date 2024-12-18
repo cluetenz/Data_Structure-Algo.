3c91d78199e0c00a88708382d6ed6008408411ab
Piyush Kumar <piyushcaptian0@gmail.com>
2024-12-19 02:26:01 +0530

    Update 1570-final-prices-with-a-special-discount-in-a-shop.java

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,3 +1,6 @@
+//Approch 1:- Brute Force
+//T.C:-O(n^2)
+//S.C:- O(1)
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n=prices.length;
@@ -12,5 +15,23 @@ class Solution {
             }
         }
         return res;
+    }
+}
+
+//Approch 2:- Using Monotonic Stack
+//T.C:-O(n)
+//S.C:-O(n)
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n=prices.length;
+        Stack<Integer> st=new Stack<>();
+        int[] result = prices.clone();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && prices[i]<=prices[st.peek()]){
+                result[st.pop()]-=prices[i];
+            }
+            st.push(i);
+        }
+        return result;
     }
 }

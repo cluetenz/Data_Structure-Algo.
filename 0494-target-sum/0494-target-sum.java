75d7941886e3b74e5a80cfb859b8d28bd16e0e38
Piyush Kumar <piyushcaptian0@gmail.com>
2024-12-27 02:46:33 +0530

    Update 0494-target-sum.java

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,4 +1,4 @@
-//Using memomization
+//Using map memomization
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         HashMap<String,Integer> map=new HashMap<>();
@@ -23,5 +23,47 @@ class Solution {
         map.put(key, plus + minus);
 
         return map.get(key);
+    }
+}
+
+//using array memomization
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        int offset = sum;
+        int[][] memo = new int[n + 1][2 * sum + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < 2 * sum + 1; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        
+        return solve(nums, 0, 0, target, memo, offset);
+    }
+
+    public int solve(int[] nums, int idx, int currSum, int target, int[][] memo, int offset) {
+        if (idx == nums.length) {
+            return currSum == target ? 1 : 0;
+        }
+
+        int adjustedSumIndex = currSum + offset;
+
+        if (memo[idx][adjustedSumIndex] != -1) {
+            return memo[idx][adjustedSumIndex];
+        }
+
+        int plus = solve(nums, idx + 1, currSum + nums[idx], target, memo, offset);
+        int minus = solve(nums, idx + 1, currSum - nums[idx], target, memo, offset);
+
+        memo[idx][adjustedSumIndex] = plus + minus;
+
+        return memo[idx][adjustedSumIndex];
     }
 }

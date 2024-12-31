2e29db671a4d3464030298f86d6271a9a9dc39bb
Piyush Kumar <piyushcaptian0@gmail.com>
2024-12-31 06:55:17 +0530

    Update 2562-count-ways-to-build-good-strings.java

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
@@ -36,3 +36,29 @@ class Solution {
         return count;
     }
 }
+//bootum up approch (dp)
+
+class Solution {
+    private final int mod = 1000000007;
+
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        int dp[] = new int[high + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= high; i++) {
+            if (i - zero >= 0) {
+                dp[i] = (dp[i] + dp[i - zero]) % mod;
+            }
+            if (i - one >= 0) {
+                dp[i] = (dp[i] + dp[i - one]) % mod;
+            }
+        }
+
+        int ans = 0;
+        for (int i = low; i <= high; i++) {
+            ans = (ans + dp[i]) % mod;
+        }
+
+        return ans;
+    }
+}

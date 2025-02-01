77bc4be6f0a31d1d5384cf1f99e0127597e7da84
Piyush Kumar <piyushcaptian0@gmail.com>
2025-02-01 15:18:42 +0530

    Update 3429-special-array-i.java

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,3 +1,6 @@
+//AAproch:- simple loop
+//T.C:- O(n)
+//S.C:- O(1)
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         int n=nums.length;
@@ -8,6 +11,29 @@ class Solution {
         int j=1;
         while(j<n){
             if(nums[i]%2==nums[j]%2){
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
+
+
+//Approch:-bit manipulation
+//T.C:-O(n)
+//S.C:-O(1)
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return true;
+        }
+        int i=0;
+        int j=1;
+        while(j<n){
+            if((nums[i]&1)==(nums[j]&1)){
                 return false;
             }
             i++;

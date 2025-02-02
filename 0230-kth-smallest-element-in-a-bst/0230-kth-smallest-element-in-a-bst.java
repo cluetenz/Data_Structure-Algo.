525e90a214b67faf028560f94da4e5d1f1b73fbf
Piyush Kumar <piyushcaptian0@gmail.com>
2025-02-02 14:05:19 +0530

    Update 0230-kth-smallest-element-in-a-bst.java

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -32,3 +32,26 @@ class Solution {
         solve(root.right,heap);
     }
 }
+//Apporch:- simple by inorder and reducing k
+//T.C:- 
+class Solution {
+    private int count = 0;
+    private int result = -1;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return result;
+    }
+
+    private void inorder(TreeNode node, int k) {
+        if (node == null) return;
+
+        inorder(node.left, k);
+        count++;
+        if (count == k) {
+            result = node.val;
+            return;
+        }
+        inorder(node.right, k);
+    }
+}

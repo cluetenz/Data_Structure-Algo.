f19b49b3598a2e254ea21fb0a622d8db6852aac6
Piyush Kumar <piyushcaptian0@gmail.com>
2025-02-11 02:20:12 +0530

    Update 3447-clear-digits.java

--- 3447-clear-digits/3447-clear-digits.java
+++ 3447-clear-digits/3447-clear-digits.java
@@ -1,3 +1,6 @@
+//Approach-1 (Brute Force)
+//T.C : O(n^2)
+//S.C : O(1)
 class Solution {
     public String clearDigits(String s) {
         StringBuilder sb = new StringBuilder(s);
@@ -17,3 +20,42 @@ class Solution {
         return sb.toString();
     }
 }
+//Approch:-Using Stack
+//T.C:- O(n)
+//S.C:-O(n)
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch>='a'&&ch<='z'){
+                st.push(ch);
+            }else if(!st.isEmpty()){
+                st.pop();
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        
+        return sb.reverse().toString();
+    }
+}
+// Approach-3 (avoid reverse call)  
+// T.C : O(n)  
+// S.C : O(1)  
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            if(ch>='a'&&ch<='z'){
+                sb.append(ch);
+            }else if(sb.length()>0){
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }    
+        return sb.toString();
+    }
+}
+
+

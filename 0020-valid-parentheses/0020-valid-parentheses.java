de1da84620e4d6c9082690c98b1d47f70f3833de
Piyush Kumar <piyushcaptian0@gmail.com>
2025-02-11 23:41:48 +0530

    Update 0020-valid-parentheses.java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,3 +1,4 @@
+//Approch:-Using Stack
 class Solution {
     public boolean isValid(String s) {
         int n = s.length();
@@ -29,3 +30,34 @@ class Solution {
         return st.isEmpty();
     }
 }
+//Approch:-Using StringBuilder
+
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            
+            if (ch == '(' || ch == '{' || ch == '[') {
+                sb.append(ch); // Push to "stack"
+            } else {
+                int len = sb.length();
+                if (len == 0) return false; // No opening bracket to match
+                
+                char last = sb.charAt(len - 1);
+                
+                if ((ch == ')' && last == '(') || 
+                    (ch == '}' && last == '{') || 
+                    (ch == ']' && last == '[')) {
+                    sb.setLength(len - 1); // Pop from "stack"
+                } else {
+                    return false;
+                }
+            }
+        }
+        return sb.length() == 0; // Stack should be empty
+    }
+}
+

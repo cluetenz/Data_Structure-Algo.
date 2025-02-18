ce5d1ffcc2d95eaa10e3aaeb36fc2aafd2599614
Piyush Kumar <piyushcaptian0@gmail.com>
2025-02-19 03:34:08 +0530

    Time: 21 ms (9.12%), Space: 41.7 MB (62.38%) - LeetHub

---
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -0,0 +1,60 @@
+class Solution {
+    public String smallestNumber(String pattern) {
+        int n = pattern.length();
+        char[] num = new char[n + 1];
+        
+        for (int i = 0; i < n + 1; i++) {
+            num[i] = (char) ('1' + i);
+        }
+        
+        do {
+            if (isValid(num, pattern)) {
+                return new String(num);
+            }
+        } while (nextPermutation(num));
+        
+        return "";
+    }
+    private boolean nextPermutation(char[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i < 0) return false;
+        
+        int j = nums.length - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        swap(nums, i, j);
+        reverse(nums, i + 1, nums.length - 1);
+        return true;
+    }
+    
+    private void swap(char[] nums, int i, int j) {
+        char temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(char[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+    
+    private boolean isValid(char[] num, String pattern) {
+        for (int i = 0; i < pattern.length(); i++) {
+            if (pattern.charAt(i) == 'I' && num[i] > num[i + 1]) {
+                return false;
+            }
+            if (pattern.charAt(i) == 'D' && num[i] < num[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
